5b2c94e1b086fe20d546789960da1ca17720f237
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-09-15 11:23:19 -0700

    Update test_from_dicom_custom_gpu.py

--- test_from_dicom_custom_gpu.py
+++ test_from_dicom_custom_gpu.py
@@ -22,7 +22,7 @@ print("is study window empty")
 print(study_windows)
 
 print ("Loading saved model...")
-model, ckpt_info = ModelSaver.load_model(ckpt_path, '1')
+model, ckpt_info = ModelSaver.load_model(ckpt_path, '0')
 
 print ("Sending model to GPU device...")
 #start_epoch = ckpt_info['epoch'] + 1
