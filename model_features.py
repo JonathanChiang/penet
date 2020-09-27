5e48e1871339a3d5650bdb98192bed4b474c8658
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-09-27 12:05:22 -0700

    Create model_features.py

---
+++ model_features.py
@@ -0,0 +1,36 @@
+"""Take in DICOM file and output CTPA to into Model
+"""
+
+import argparse
+import util
+import torch
+from saver import ModelSaver
+from models import *
+
+input_study = '/data/location/'
+series_description="CTA 2.0 CTA/PULM CE"
+ckpt_path = 'penet_best.pth.tar'
+device = 'cuda'
+gpu_ids = 1 
+#map_location = 'cpu'
+print("Reading input dicom...")
+study = util.dicom_2_npy(input_study)
+print('is study empty')
+print(study)
+# normalize and convert to tensor
+print("Formatting input for model...")
+study_windows = util.format_img(study) 
+print("is study window empty")
+print(study_windows)
+
+print ("Loading saved model...")
+model, ckpt_info = ModelSaver.load_model(ckpt_path, [0])
+
+print ("Sending model to GPU device...")
+#start_epoch = ckpt_info['epoch'] + 1
+model = model.to(device)
+
+num_ftrs = model.fc.in_features
+print(num_ftrs)
+
+
